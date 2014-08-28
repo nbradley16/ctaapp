534422ee3d20fa33f80c3b4f8eb2318b3e1896ef
nbradley616@gmail.com <>
2014-08-28 22:28:29 +0000

    init

---
+++ js/pull2.js
@@ -0,0 +1,35 @@
+$.getJSON('js/busstops.json', function(busstops){  
+  $('#busstoplist').empty();
+  
+  $.each(busstops, function(i,busstop){
+      $('#busstoplist').append(generateBusStopLink(busstop));
+   });
+   
+   $('#busstoplist').listview('refresh');
+   
+});
+
+function generateLStopLink(busstop){
+
+    return'<li><a href="javascript:void(0)'
+           + '"onclick="goToStopDetailPage(\''
+           + busstop.stop_name
+           +'\',\''
+           + busstop.station_descriptive_name +'\')">'
+           + busstop.stop_name
+           + '</a></li>';
+}
+
+function goToStopDetailPage(busstopName){
+
+    //create the page html template
+   var busstopPage =$("<div data-role='page' data-url=station><div data-role='header'><h1>"
+                  + busstopName + "</h1></div><div data-role='content'></div><div data-role='footer'><h4>"
+                  + busstopName + "</h4></div></div>");
+                  
+//append the new page to the page container
+busstopPage.appendTo( $.mobile.pageContainer );
+
+//go to the newly create page
+$.mobile.changePage( busstopPage);
+}
